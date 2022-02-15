7947b20a413b53c3e998548d1bcd6d418ab88ece
Peter92Xiong <peter92xiong@gmail.com>
2022-02-15 18:42:33 -0500

    a start to making 6 lucky numbers

---
+++ LucklyLottNums.py
@@ -0,0 +1,15 @@
+import random
+
+x = random.randrange(1,69);
+
+luckyNums = [0,0,0,0,0,0]
+
+#luckyNums.insert(x)
+
+print(x)
+
+for i in range(4):
+    x = random.randrange(1, 69);
+
+
+print(luckyNums)
