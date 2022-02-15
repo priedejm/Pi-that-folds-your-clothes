0190c6bc8d361be9133ba77bcac4ea1529e0d9ad
priedejm <priedejm@appstate.edu>
2022-02-15 14:26:34 -0500

    reduces jitter

--- servo.py
+++ servo.py
@@ -1,7 +1,12 @@
 from gpiozero import Servo
 from time import sleep
 
-servo = Servo(12)
+from gpiozero.pins.pigpio import PiGPIOFactory
+#comment out factory and take it out the servo to see it work worse.
+factory = PiGPIOFactory()
+#pin_factory=factory
+
+servo = Servo(12, pin_factory=factory)
 
 print("Start in the middle")
 servo.mid()
