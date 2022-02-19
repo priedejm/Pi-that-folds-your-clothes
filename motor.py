d57f57d45a8e18cfbcc0792b9f107fa4f9efc96e
priedejm <78455131+priedejm@users.noreply.github.com>
2022-02-19 15:36:39 -0500

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,6 +1,24 @@
-import RIp.GPIO as gpio
+import RPi.GPIO as gpio
+import time
 
 def init():
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
-    gpio.setup(22, gpio.OUT)
+
+def forward(tf):
+    init()
+    gpio.output(17, True)
+    time.sleep(tf)
+    gpio.cleanup()
+
+def reverse(tf):
+    init()
+    gpio.output(17, False)
+    time.sleep(tf)
+    gpio.cleanup()
+
+print ("forward")
+forward(4)
+print ("backward")
+reverse(2)
+
