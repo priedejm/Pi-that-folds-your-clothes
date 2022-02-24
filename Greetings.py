9f57240c2c157d44be41c75a9f3620da1b6db5ef
Peter92Xiong <peter92xiong@gmail.com>
2022-02-23 21:33:01 -0500

    start up menu

---
+++ Greetings.py
@@ -0,0 +1,24 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+print("Welcome to the pi that will fold your cloths for you\nWhat would you like to do?")
+print("1)Fold cloth\n2)My stats\n3)Today's lucky numbers\n4)A joke")
+
+#4 buttons when press will do one of each of these
+
+
