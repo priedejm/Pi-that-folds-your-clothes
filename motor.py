4e31640d2d07206ad74cd5e8e54ec0ab46469d59
priedejm <priedejm@appstate.edu>
2022-03-01 20:57:10 -0500

    update

--- motor.py
+++ motor.py
@@ -5,23 +5,33 @@ def init():
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
     gpio.setup(22, gpio.OUT)
+    gpio.setup(23, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
 
 def forward(tf):
     init()
     gpio.output(17, True)
     gpio.output(22, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
     time.sleep(tf)
+ 
     gpio.cleanup()
 
 def reverse(tf):
     init()
-    gpio.output(17,False )
+    gpio.output(17,False)
     gpio.output(22, True)
+    gpio.output(23, False)
+    gpio.output(24, True)
     time.sleep(tf)
     gpio.cleanup()
 
-print ("forward")
-forward(2)
-print ("backward")
-reverse(2)
+for i in range(0,10):
+    print ("forward")
+    forward(.015)
+    time.sleep(1)
+    print ("backward")
+    reverse(.015)
+    time.sleep(1)
 
