50f4a0831cd8fee2b1119da66d8fe2c7dfdf6c92
priedejm <priedejm@appstate.edu>
2022-03-14 22:21:58 -0400

    PWM

---
+++ PWMSimple.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+ 
+GPIO.setmode(GPIO.BCM) 
+ 
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+ 
+direction = input('Please define the direction (Left=1 or Right=2): ')
+dc = input('Please define the Motor PWM Duty Cycle (0-100): ')
+hz = input ('HZ: ')
+pwm = GPIO.PWM(17, hz)
+ 
+if direction == 1:
+    GPIO.output(27, 1)
+    GPIO.output(22, 0)
+elif direction == 2:
+    GPIO.output(27, 0)
+    GPIO.output(22, 1)
+ 
+try:
+        while True:
+                pwm.start(dc)
+ 
+except KeyboardInterrupt:
+        pwm.stop()
+        GPIO.cleanup()
