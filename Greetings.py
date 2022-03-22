b579063fd30426164865289e4fb9133ef8a8ac8f
Peter92xiong <Peter92xiong@gmail.com>
2022-03-21 22:42:57 -0400

    option 3 and 4 are working correctly

--- Greetings.py
+++ Greetings.py
@@ -14,10 +14,10 @@
 #     print_hi('PyCharm')
 #
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+import random
 
 #geezz python has such simplictly syntax which makes it so diffcult in some cases. HOW DO YOU RUN THE MAIN FUNCTION
-main()
+
 
 def main():
     #4 buttons when press will do one of each of these
@@ -46,6 +46,8 @@ def main():
         print("Error: getting input from user stage")
 
 
+
+
 def foldClothFunction():
     print()
     #will go into this function doing some cool stuff with more options
@@ -56,47 +58,106 @@ def luckyNumsFunction():
     print()
     #go to the luckyNums file.py
 
+
+    # x = random.randrange(1,69);
+    #
+    # luckyNums = [0,0,0,0,0,0]
+    #
+    # luckyNums.insert(0,x)
+    #
+    # print(x)
+    #
+    # for i in range(4):
+    #     x = random.randrange(1, 69);
+
+    # print(luckyNums)
+    luckyNumAgain = 0
+    while luckyNumAgain == 0:
+        luckyNumbers = []
+
+        for i in range(0, 5):
+            whiteball = random.randint(1, 69)
+            # if the number is in the list. do while loop again until the number is not in the list
+            while whiteball in luckyNumbers:
+                luckyNum = random.randint(1, 69)
+            luckyNumbers.append(whiteball)
+        # sort the 5 lucky numbers
+        luckyNumbers.sort()
+        # redball does not need to be sorted.
+        redball = random.randint(1, 26)
+        luckyNumbers.insert(6, redball)
+        print("Hi, your lucky numbers for the week are: ")
+        print(luckyNumbers)
+        print("Please play responsibly")
+        print(
+            "If you have a gambling addiction please call the National Problem Gambling Helpline Network phone number at 1-800-522-4700")
+        print("Help is available 24/7 and is 100% confidential.")
+        print("The National Problem Gambling Helpline Network also includes text and chat services.")
+        print()
+        print()
+        print("Would you like another set of lucky number? yes or no")
+        userinput = input()
+        if userinput == "yes":
+            luckyNumAgain = 0;
+        if userinput == "no":
+            main()
+
 def aJokeFunction():
     #going to have 100+ jokes and store them somewhere else.
     #random number generator
     #switch statement....or something
-
-    print("what's a ghost's favourite type?")
-    print("Booooolean")
-
-    print("student: My dog ate my homework")
-    print("Teacher: But it's an online homework")
-    print("student: yes, he took quite a byte out of it")
-
-    print("There are 10 types of people in this world.\nThose who understand binary and those who don't.")
-
-    print("There are 10 types of people in this world.")
-    print("Those who understand binary and those who don't.. and those who know this is a trinary joke.")
-
-    print("Why did the programmer quit his job?")
-    print("because he didn't get arrays")
-
-    print("How many software engineer does it take to change a light bulb?")
-    print("none, that's a hardware problem")
-
-
-    print("I've got a really good UDP joke to tell you, but I don't think you'll get it")
-
-    print("What's the best part about TCP jokes?\nI get to keep telling them until you get them.")
-
-    print("A guy walks into a bar and asks for 1.4 root beers. The bartender says \"I'll have to charge you extra, that's a root beer float\".")
-    print("The guy says \"In that case, better make it a double.\"")
-
-    print("Java and C++ were telling jokes. It was C++'s turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.")
-
-    print("wife: could you pass the salt, dear")
-    print("husband: by value or by reference?")
-
-    print("In order to understand recursion you must first understand recursion.")
-
-
-    print("how do SQL developer get into their house?\nWith keys")
-
-    print("What is a programmers favorite exercise activity?\nSprint") #px
     
-    print("Why do IT people wear glasses\nBecause they don't C#")
+    #print(ranNum)
+    tellAJokeAgain = 0
+    while tellAJokeAgain == 0:
+        ranNum = random.randint(1,16)
+        if ranNum == 1:
+            print("what's a ghost's favourite type?")
+            print("Booooolean")
+        if ranNum == 2:
+            print("student: My dog ate my homework")
+            print("Teacher: But it's an online homework")
+            print("student: yes, he took quite a byte out of it")
+        if ranNum == 3:
+            print("There are 10 types of people in this world.\nThose who understand binary and those who don't.")
+        if ranNum == 4:
+            print("There are 10 types of people in this world.")
+            print("Those who understand binary and those who don't.. and those who know this is a trinary joke.")
+        if ranNum == 5:
+            print("Why did the programmer quit his job?")
+            print("because he didn't get arrays")
+        if ranNum == 6:
+            print("How many software engineer does it take to change a light bulb?")
+            print("none, that's a hardware problem")
+        if ranNum == 7:
+            print("I've got a really good UDP joke to tell you, but I don't think you'll get it")
+        if ranNum == 8:
+            print("What's the best part about TCP jokes?\nI get to keep telling them until you get them.")
+        if ranNum == 9:
+            print("A guy walks into a bar and asks for 1.4 root beers. The bartender says \"I'll have to charge you extra, that's a root beer float\".")
+            print("The guy says \"In that case, better make it a double.\"")
+        if ranNum == 10:
+            print("Java and C++ were telling jokes. It was C++'s turn, so he writes something on the wall, points to it and says \"Do you get the reference?\" But Java didn't.")
+        if ranNum == 11:
+            print("wife: could you pass the salt, dear")
+            print("husband: by value or by reference?")
+        if ranNum == 12:
+            print("In order to understand recursion you must first understand recursion.")
+        if ranNum == 13:
+            print("how do SQL developer get into their house?\nWith keys")
+        if ranNum == 14:
+            print("What is a programmers favorite exercise activity?\nSprint")
+        if ranNum == 15:
+            print("Why do IT people wear glasses\nBecause they don't C#")
+        if ranNum == 16:
+            print("why was there no windows 9?")
+            print("because windows 7 8 9.")
+        print("Would you like to hear another joke? yes or no")
+        userinput = input()
+        if userinput == "yes":
+            tellAJokeAgain = 0;
+        if userinput == "no":
+            main()
+
+
+main()
