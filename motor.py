2f60d3748c5639a137e9a33e4f0141e22cc1c601
Peter92Xiong <79063294+Peter92Xiong@users.noreply.github.com>
2022-04-25 22:53:40 -0400

    Update motor.py

--- motor.py
+++ motor.py
@@ -3,32 +3,67 @@ import time
 
 
 gpio.setmode(gpio.BCM)
-gpio.setup(17, gpio.OUT) #right motor
-gpio.setup(22, gpio.OUT) #right motor
-gpio.setup(23, gpio.OUT) #left motor
-gpio.setup(24, gpio.OUT) #left motor
+gpio.setup(17, gpio.OUT) #left motor
+gpio.setup(27, gpio.OUT) #left motor
+gpio.setup(23, gpio.OUT) #right motor
+gpio.setup(24, gpio.OUT) #right motor
 gpio.setup(6, gpio.OUT) #middle motor
 gpio.setup(13, gpio.OUT) #middle motor
 
 
-# def forward(tf):
-#     init()
-#     gpio.output(17, True)
-#     gpio.output(22, False)
-#     gpio.output(23, True)
-#     gpio.output(24, False)
-#     time.sleep(tf)
- 
-#     gpio.cleanup()
+def moveAllMotors():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(17, gpio.OUT) #left motor
+    gpio.setup(27, gpio.OUT) #left motor
+    gpio.setup(23, gpio.OUT) #right motor
+    gpio.setup(24, gpio.OUT) #right motor
+    gpio.setup(6, gpio.OUT) #middle motor
+    gpio.setup(13, gpio.OUT) #middle motor
+    #left motors:
+    gpio.output(17, True)
+    gpio.output(27, False)
+    time.sleep(.7)
+    gpio.output(17,False)
+    gpio.output(27, True)
+    time.sleep(.7)
 
-# def reverse(tf):
-#     init()
-#     gpio.output(17,False)
-#     gpio.output(22, True)
-#     gpio.output(23, False)
-#     gpio.output(24, True)
-#     time.sleep(tf)
-#     gpio.cleanup()
+    #right motors:
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(.7)
+    gpio.output(23,False)
+    gpio.output(24, True)
+    time.sleep(.7)
+
+    #Middle motor:
+    gpio.output(6, True)
+    gpio.output(13, False)
+    time.sleep(.7)
+    gpio.output(6,False)
+    gpio.output(13, True)
+    time.sleep(.7)
+    gpio.cleanup()
+
+
+def forwardAll():
+    gpio.output(17, True)
+    gpio.output(27, False)
+    gpio.output(23, True)
+    gpio.output(24, False)
+    gpio.output(6, True)
+    gpio.output(13, False)
+    time.sleep(.7)
+    gpio.cleanup()
+
+def reverseAll():
+    gpio.output(17,False)
+    gpio.output(27, True)
+    gpio.output(23,False)
+    gpio.output(24, True)
+    gpio.output(6,False)
+    gpio.output(13, True)
+    time.sleep(.7)
+    gpio.cleanup()
 
 # for i in range(0,2):
 #     print ("forward")
@@ -40,36 +75,34 @@ gpio.setup(13, gpio.OUT) #middle motor
 #     gpio.cleanup()
 # GPIO.cleanup()
 
-# def rightMotor():
-#     #forward
-#     gpio.output(17, True)
-#     gpio.output(22, False)
-#     time.sleep(.2)
-#     #reverse
-#     gpio.output(17,False)
-#     gpio.output(22, True)
-#     time.sleep(.2)
+def rightMotor():
+    gpio.output(23, True)
+    gpio.output(24, False)
+    time.sleep(.7)
+    gpio.output(23,False)
+    gpio.output(24, True)
+    time.sleep(.7)
+    gpio.cleanup()
    
 
 
-# def leftMotor():
-#     #forward
-#     gpio.output(23, True)
-#     gpio.output(24, False)
-#     time.sleep(.2)
-#     #reverse
-#     gpio.output(23,False)
-#     gpio.output(24, True)
-#     time.sleep(.2)
+def leftMotor():
+    gpio.output(17, True)
+    gpio.output(27, False)
+    time.sleep(.7)
+    gpio.output(17,False)
+    gpio.output(27, True)
+    time.sleep(.7)
+    gpio.cleanup()
+
 def middleMotor():
-    #forward
     gpio.output(6, True)
     gpio.output(13, False)
-    time.sleep(1)
-    #reverse
+    time.sleep(.7)
     gpio.output(6,False)
     gpio.output(13, True)
-    time.sleep(1)
-
-
+    time.sleep(.7)
     gpio.cleanup()
+
+
+#  gpio.cleanup()
